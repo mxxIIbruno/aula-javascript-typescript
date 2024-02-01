77e7cdb675868cd15290cf022e317e7f9ee6489e
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-01 17:43:21 -0300

    sobre variaveis

---
+++ js/aula6/index.js
@@ -0,0 +1,16 @@
+// let name = 'João'; // no outro caso poderia usar var, porem é mais antigo
+
+// console.log(name, 'nasceu em 1984.');
+// console.log('Em 2000', name, 'conheceu Maria.');
+// console.log(name, 'casou-se com Maria em 2012.');
+// console.log('Maria teve 1 filho com', name, 'em 2015.');
+// console.log('O filho de', name, 'chama Eduardo.');
+
+// let name; // Declarou a variável
+
+// name = 'João'; // Inicializando a variável
+// console.log(name);
+// name = 'Otávio';
+// console.log(name);
+
+
