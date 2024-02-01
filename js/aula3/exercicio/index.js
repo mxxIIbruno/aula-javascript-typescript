e427ca754f9c0bdb53a7eab826db6b65182c06f0
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-01 16:51:14 -0300

    primeiro exercicio

---
+++ js/aula3/exercicio/index.js
@@ -0,0 +1,4 @@
+console.log(
+    "Meu nome é", '"Bruno".', "Estou aprendendo JavaScript às",
+    10, 'da manhã.'
+);
---
+++ js/aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Bruno "Santos"'); // String = Texto
+console.log("Bruno 'Santos'"); // Number = Número
+console.log(`"Bruno" 'Santos'`);
+
+console.log(123456, 12.45, 'Bruno Santos de Novo');
