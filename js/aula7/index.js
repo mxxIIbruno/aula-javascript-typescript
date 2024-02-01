de2036eaf1fe90d94cc84dfe9f931529f6ec3510
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-01 18:03:02 -0300

    sobre constantes

---
+++ js/aula7/index.js
@@ -0,0 +1,13 @@
+/**
+ *  Não podemos criar constantes com paralvras reservadas
+ *  Constantes precisam ter nomes significativos
+ *  Não pode começar o nome de uma constantes com um número
+ *  Não podem conter espaços ou traços
+ *  Utilizamos camelCase
+ *  Case-senseitive
+ *  Não pode modificar o valor de uma constante
+ *  NÃO UTILIZE VAR, UTILIZE CONST.
+ */
+const nome = 'João';
+console.log(nome);
+console.log(typeof nome)
