fe11afa189354cfe1ecec0615b8fb37789e357e8
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-01 17:01:27 -0300

    aula sobre comentario em javascript

---
+++ js/aula4/index.js
@@ -0,0 +1,22 @@
+// E escrever um comentário longo
+console.log('Ola mundo!'); // Aqui tem outro comnetário
+console.log('Linha 1');
+// Comentário
+
+/* 
+Um bloco de código
+Linha de comentário.
+*/
+
+/**
+ * Um título
+ * 
+ * Lorem ipsum
+ */
+
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
+
+
