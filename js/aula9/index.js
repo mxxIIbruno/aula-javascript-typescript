4a5be4dda3f1f8ce1d5d0c4c7219b92c3b365f62
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 00:30:21 -0300

    aula sobre dados primitivos

---
+++ js/aula9/index.js
@@ -0,0 +1,23 @@
+const nome = 'Bruno'; // string
+const nome1 = "Bruno"; // string
+const nome2 = `Bruno`; // string
+const num1 = 10; // number
+const num2 = 10.52 // number
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
+const aprovado = true; // Boolean = true, false (lógico)
+
+// const a = [1, 2];
+// const b = a;
+
+// console.log(a, b);
+
+// b.push(3);
+// console.log(a, b)
+
+let a = 2;
+let b = a;
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
