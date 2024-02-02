1df8552d3f5ba32e9842b01c7d3b621e58aacfcf
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 02:07:06 -0300

    aula sobre os metodos que manipula uma string

---
+++ js/aula13/index.js
@@ -0,0 +1,20 @@
+// let umaString = "Um \"texto\"";
+// let umaString = "Um \\texto";
+let umaString = "O rato roeu a roupa do rei de roma.";
+// console.log(umaString.charAt(6)); // Output: t
+// console.log(umaString.concat(' em um dia.'));
+// console.log(umaString.indexOf('texto'));
+// console.log(umaString.indexOf('texto', 3));
+// console.log(umaString.lastIndexOf('m', 3));
+// console.log(umaString.match(/[a-z]/g));
+// console.log(umaString.search(/x/));
+// console.log(umaString.replace(/Um/, 'Outra'));
+// console.log(umaString.replace(/r/, '#'));
+// console.log(umaString.replace(/r/g, '#'));
+// console.log(umaString.length);
+// console.log(umaString.slice(2, 5));
+// console.log(umaString.slice(-3));
+// console.log(umaString.substring(umaString.length - 5, umaString.length - 1));
+// console.log(umaString.split(' ', 2));
+// console.log(umaString.toLowerCase());
+console.log(umaString.toUpperCase());
