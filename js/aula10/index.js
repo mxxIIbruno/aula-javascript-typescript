3b09eb77804168340ae2734f7989aa6f18a0bc4a
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 00:59:31 -0300

    aula sobre operadores

---
+++ js/aula10/index.js
@@ -0,0 +1,20 @@
+/*
+Aritméticos -> 
++ Adição / Concatenação ( - / *)
+** Potenciação
+  % Resto da divisão
+ 
+  Ordem
+1. ()
+2. **
+3. * / %
+4. + -
+
+Incremento = ++
+Decremento = --
+ */
+const passo = 2;
+let contador = parseInt('15');
+// contador += passo; // contador = contador + passo
+console.log(contador + passo);
+// NaN - Not a number parseInt (inteiro), parseFloat (decimais)
