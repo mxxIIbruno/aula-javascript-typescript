95bfe140e2d1cc35887c008f9d784948ced0e3fe
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 19:51:58 -0300

    aula sobre Math

---
+++ js/aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9.49;
+// let num1 = 9.499_999_999_999_999_99;
+// let num2 = Math.floor(num1); <
+// let num2 = Math.ceil(num1); >
+// let num2 = Math.round(num1);
+// console.log(Math.max((1,2,3,4,5,6,7,8,9,456)));
+// console.log(Math.min((1,2,3,4,5,6,7,8,9,456)));
+
+// const aleatorio = Math.random() * (10 - 5) + 5;
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(Math.sqrt(num1));
+console.log(100 / 0);
