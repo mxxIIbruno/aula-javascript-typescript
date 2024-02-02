b6b7014c65a36008e0cee8e77b396c3b71b4f75b
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 19:32:52 -0300

    aula sobre number e os metodos

---
+++ js/aula14/index.js
@@ -0,0 +1,25 @@
+// IEEE 754-2008
+let num1 = 0.7; // number
+let num2 = 0.1; // number
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+// let tamp = num1 * '5';
+// console.log(Number.isNaN(tamp));
+// console.log(tamp);
+// console.log(num1 + num2);
+
+// num1 += num2;
+// num1 += num2;
+// num1 += num2;
+
+// num1 = ((num1 * 100) + (num2 * 100)) / 100;
+// num1 = ((num1 * 100) + (num2 * 100)) / 100;
+// num1 = ((num1 * 100) + (num2 * 100)) / 100;
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(Number.isInteger(num1), num1);
