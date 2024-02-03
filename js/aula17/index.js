cddad3213e27b409775db8a856274d01c5d1c88d
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 21:19:35 -0300

    aula sobre function

---
+++ js/aula17/index.js
@@ -0,0 +1,24 @@
+// function saudacao(nome) {
+//     console.log(`Bom dia, ${nome}!`);
+// }
+
+// function soma(x = 1, y = 1) {
+//     const resultado = x + y;
+//     console.log('Olá mundo!');
+//     return resultado;
+// }
+
+// console.log(soma());
+
+// const raiz = function (n) {
+//     return n ** 0.5;
+// };
+
+// const raiz = (n) => {
+//     return n ** 0.5;
+// };
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+
