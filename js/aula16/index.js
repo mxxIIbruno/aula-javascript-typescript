9deb45d1806cf18d076b5f88096f692814644b29
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 21:02:53 -0300

    aula sobre arrays e const mutaveis

---
+++ js/aula16/index.js
@@ -0,0 +1,22 @@
+const alunos = ['Bruno', 'Santos', 'Silva'];
+
+// alunos[alunos.length] = 'Tereza';
+// alunos[alunos.length] = 'Felipe';
+// alunos[alunos.length] = 'Julia';
+
+// alunos.push('Raimundo');
+// alunos.push('Outra');
+// alunos.push('Judite');
+
+alunos.unshift('Luiz');
+alunos.unshift('Kayke');
+
+// alunos.pop();
+// alunos.shift();
+
+// console.log(alunos.slice(0, 3));
+
+// console.log(typeof alunos);
+
+// console.log(alunos instanceof Array);
+console.log(alunos instanceof Array);
