7698e18b3772db288463ce810ff8b4a52097fd45
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-02 23:41:52 -0300

    exercicio para funções, array e objetos

---
+++ js/aula19/exercicio/index.js
@@ -0,0 +1,29 @@
+function meuEscopo () {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push(
+            {
+                nome: nome.value,
+                sobrenome: sobrenome.value,
+                peso: peso.value,
+                altura: altura.value
+            }
+        );
+
+        resultado.innerHTML += `<p>A pessoa ${nome.value} ${sobrenome.value} tem ${peso.value}kg e ${altura.value} altura!</p>`
+    }
+    
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
