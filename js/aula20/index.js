38952958a5efbfdb76a7a5c74472c0212c8ada1f
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-03 00:02:46 -0300

    aula sobre operadores de comparação

---
+++ js/aula20/index.js
@@ -0,0 +1,18 @@
+/*
+Operadores de comparação
+>   maior que
+>=  maior que ou igual a
+<   menor que
+<=  menor que ou igual a
+==  igualdade (valor) *********
+=== igualdade estrita (valor e tipo)
+!=  diferente (valor) *********
+!== diferente estrita (valor e tipo)
+
+*/
+const num1 = 10; // number
+// const num2 = '10'; // string
+const num2 = 10;
+const comp = num1 === num2;
+
+console.log(comp);
