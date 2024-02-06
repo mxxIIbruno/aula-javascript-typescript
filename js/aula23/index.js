f1da19c8d9f518438eb3d504698edbcb3fe5da11
mxxIIbruno <bruno.eli.lis@gmail.com>
2024-02-05 21:56:37 -0300

    aula sobre condições

---
+++ js/aula23/index.js
@@ -0,0 +1,11 @@
+const condition = 10;
+
+if (condition >= 0 && condition <= 11) {
+    console.log('Bom dia!');
+} else if (condition >= 12 && condition <= 17) {
+    console.log('Bom Tarde!');
+} else if (condition >= 18 && condition <= 23){
+    console.log('Bom Noite!');
+} else {
+    console.log('Olá');
+}
